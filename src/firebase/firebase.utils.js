39a4ddd7c030bfff71e9adab7dda4feb09e7fcb0
renatoreis <renato.reis@mjv.com.br>
2020-02-29 09:38:27 -0300

    login e criação de usuario com usario e senha

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,11 +15,44 @@ import 'firebase/auth';
 
 firebase.initializeApp(config);
 
+
+//auth
 export const auth = firebase.auth();
+//database
 export const firestore = firebase.firestore();
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+    console.log(userAuth);
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    if(!snapShot.exists) {
+        const { displayName, email, photoURL, phoneNumber } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                photoURL,
+                phoneNumber,
+                createdAt,
+                ...additionalData
+            })
+        } catch(error) {
+            console.error('error creating user', error.message);
+        }
+
+        
+    }
+    return userRef;
+    console.log(snapShot);
+}
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account'});
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+
 
 export default firebase;
