0ca3610fce5caf19f6e02ce6ede2ebccf5a2dab8
renatoreis <renato.reis@mjv.com.br>
2020-04-04 18:53:13 +0100

    Added add and remove actions

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CardActionsTypes from './card.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 const INITIAL_STATE = {
     hidden: true,
     cartItems: []
@@ -16,6 +16,16 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state, 
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            }
+        case CardActionsTypes.REMOVE_ITEM :
+            return {
+                ...state, 
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
+        case CardActionsTypes.CLEAR_ITEM_FROM_CART : 
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
             }
 
         default:
